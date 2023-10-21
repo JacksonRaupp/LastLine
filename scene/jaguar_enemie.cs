daf02c3069724dc0609f29023d1f1cbbde9cb388
AndreGomesSilva <silva_andregomes@outlook.com>
2023-10-21 19:06:59 -0300

    add function turn_arount

--- scene/jaguar_enemie.cs
+++ scene/jaguar_enemie.cs
@@ -5,12 +5,12 @@ public partial class jaguar_enemie : CharacterBody2D
 {
     public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
+    
+    public Vector2 velocity;
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
 
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
@@ -22,14 +22,22 @@ public partial class jaguar_enemie : CharacterBody2D
     public override void _Process(double delta)
     {
         var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        if (Velocity.Length() > 0)
+
+
+        TurnAround();
+        velocity.X = +Speed;
+        Velocity = Velocity.Normalized() * Speed;
+        animatedSprite2D.Play();
+    }
+
+    public void TurnAround()
+    {
+        var raycast = GetNode<RayCast2D>("RayCast2D");
+
+        if (!raycast.IsColliding())
         {
-            Velocity = Velocity.Normalized() * Speed;
-            animatedSprite2D.Play();
-        }
-        else
-        {
-            animatedSprite2D.Stop();
+            GD.Print("tumdum");
         }
     }
+
 }
